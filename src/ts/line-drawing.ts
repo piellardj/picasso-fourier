678406c279e67258d697f58f76966acd99938ac0
Jérémie Piellard <jeremie.piellard@gmail.com>
2019-12-15 19:33:03 +0100

    Fourier integration is now more precise
    Previously, it supposed the points were evenly spaced so if they were not, the path samples were off.
    it is no longer the case and the path samples are now sampled at a regular distance no matter the points distribution.

--- src/ts/line-drawing.ts
+++ src/ts/line-drawing.ts
@@ -11,47 +11,61 @@ class LineDrawing {
         };
     }
 
+    private static distance(p1: IPoint, p2: IPoint): number {
+        const dX = p1.x - p2.x;
+        const dY = p1.y - p2.y;
+        return Math.sqrt(dX * dX + dY * dY);
+    }
+
     private readonly points: IPoint[];
-    private readonly nbPoints: number;
     public readonly pathLength: number;
 
     public constructor() {
-        this.points = [];
-
         this.points = Presets.Dog;
-        this.nbPoints = this.points.length - 1;
 
         this.pathLength = 0;
-        let diffX;
-        let diffY;
-        for (let i = 0; i < this.points.length - 2; i++) {
-            diffX = this.points[i].x - this.points[i + 1].x;
-            diffY = this.points[i].y - this.points[i + 1].y;
-            this.pathLength += Math.sqrt(diffX * diffX + diffY * diffY);
+        for (let i = 0; i < this.points.length - 1; i++) {
+            this.pathLength += LineDrawing.distance(this.points[i], this.points[i + 1]);
         }
     }
 
     /* Assumes t is between 0 and 1 included. */
     public draw(context: CanvasRenderingContext2D, t: number) {
         context.beginPath();
-
         context.moveTo(this.points[0].x, this.points[0].y);
 
-        const fullUntil = Math.floor(t * this.nbPoints);
-        for (let i = 0; i <= fullUntil; i++) {
-            context.lineTo(this.points[i].x, this.points[i].y);
+        const desiredLength = t * this.pathLength;
+
+        let currentLength = 0;
+
+        let i: number;
+        for (i = 0; i < this.points.length - 1; i++) {
+            const lastPoint = this.points[i];
+            const nextPoint = this.points[i + 1];
+            const segmentLength = LineDrawing.distance(lastPoint, nextPoint);
+
+            if (currentLength + segmentLength < desiredLength) {
+                currentLength += segmentLength;
+                context.lineTo(nextPoint.x, nextPoint.y);
+            } else {
+                const interpolationFactor = (desiredLength - currentLength) / segmentLength;
+                const finalPoint = LineDrawing.interpolate(lastPoint, nextPoint, interpolationFactor);
+                context.lineTo(finalPoint.x, finalPoint.y);
+                currentLength += LineDrawing.distance(lastPoint, finalPoint);
+                break;
+            }
         }
 
-        const finalPoint = this.computePoint(t);
-        context.lineTo(finalPoint.x, finalPoint.y);
+        console.log(i + " current: " + currentLength + " / " + desiredLength);
 
         context.stroke();
         context.closePath();
     }
 
     public computeFourierSeries(order: number): FourierSeries {
-        const integrationPrecision = Math.ceil(this.pathLength);
-        const precision = 1 / integrationPrecision;
+        const nbSteps = Math.ceil(0.5 * this.pathLength); // number of integration steps
+        const stepSize = this.pathLength / nbSteps;
+        const dT = 1 / nbSteps;
 
         /* Precompute function samples to avoid computing them for each coefficient. */
         interface IFunctionSample {
@@ -61,19 +75,38 @@ class LineDrawing {
         }
 
         const samples: IFunctionSample[] = [];
-        for (let i = 0; i < integrationPrecision; i++) {
-            const t = (i + 0.5) * precision;
-            const point = this.computePoint(t);
-            samples.push({
-                x: point.x,
-                y: point.y,
-                two_pi_t: 2 * Math.PI * t,
-            });
+        {
+            let currentLength = 0;
+            let lastPointIndex = 0;
+            for (let iStep = 0; iStep < nbSteps; iStep++) {
+                const desiredT = (iStep + 0.5) * dT;
+                const desiredLength = (iStep + 0.5) * stepSize;
+
+                for (let iPoint = lastPointIndex; iPoint < this.points.length - 1; iPoint++) {
+                    const lastPoint = this.points[iPoint];
+                    const nextPoint = this.points[iPoint + 1];
+                    const segmentLength = LineDrawing.distance(lastPoint, nextPoint);
+
+                    if (currentLength + segmentLength < desiredLength) {
+                        currentLength += segmentLength;
+                        lastPointIndex = iPoint + 1;
+                    } else {
+                        const interpolationFactor = (desiredLength - currentLength) / segmentLength;
+                        const exactPoint = LineDrawing.interpolate(lastPoint, nextPoint, interpolationFactor);
+                        samples.push({
+                            x: exactPoint.x,
+                            y: exactPoint.y,
+                            two_pi_t: 2 * Math.PI * desiredT,
+                        });
+                        break;
+                    }
+                }
+            }
         }
 
         const coefficients: IFourierCoefficient[] = [];
         for (let i = 0; i < 2 * order + 1; i++) {
-            let n = Math.floor((i + 1) / 2);
+            let n = Math.floor((i + 1) / 2); // n is the index of the coefficient we're about to compute
             if (i > 0 && i % 2 === 0) {
                 n *= -1;
             }
@@ -85,8 +118,8 @@ class LineDrawing {
                 const cos = Math.cos(TWO_PI_N_T);
                 const sin = Math.sin(TWO_PI_N_T);
 
-                cx += precision * (sample.x * cos + sample.y * sin);
-                cy += precision * (sample.y * cos - sample.x * sin);
+                cx += dT * (sample.x * cos + sample.y * sin);
+                cy += dT * (sample.y * cos - sample.x * sin);
             }
 
             coefficients.push({
@@ -97,14 +130,6 @@ class LineDrawing {
         }
 
         return new FourierSeries(coefficients);
-    }
-
-    /* Assumes t is between 0 and 1 included. */
-    private computePoint(t: number): IPoint {
-        const index = t * this.nbPoints;
-        const floorIndex = Math.floor(index);
-
-        return LineDrawing.interpolate(this.points[floorIndex], this.points[floorIndex + 1], index - floorIndex);
     }
 }
 
